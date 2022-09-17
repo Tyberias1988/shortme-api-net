7d7e194dfd55d18782d63fe0377ec16c0c409fc9
Benjamin Strecker <info@strecker-it.net>
2022-09-17 14:37:58 +0200

    Fehler bei GetAllShortLinks gefixed

--- shortme-api-net/Services/ShortLinkService.cs
+++ shortme-api-net/Services/ShortLinkService.cs
@@ -6,6 +6,7 @@ namespace shortme_api_net.Services;
 public interface IShortLinkService
 {
     Task<List<ShortLink>> GetAllShortLinks();
+    ShortLink Create(ShortLink shortLink);
 }
 
 public class ShortLinkService : IShortLinkService
@@ -20,8 +21,16 @@ public class ShortLinkService : IShortLinkService
 
     public async Task<List<ShortLink>> GetAllShortLinks()
     {
-        var links = await _applicationDbContext.ShortLinks.Where(s => s.CreatedAt > DateTime.Now).ToListAsync();
+        //var links = await _applicationDbContext.ShortLinks.Where(s => s.CreatedAt > DateTime.Now).ToListAsync();
+        var links = await _applicationDbContext.ShortLinks.ToListAsync();
         return links;
     }
-    
+
+    public ShortLink Create(ShortLink newShortLink)
+    {
+        _applicationDbContext.ShortLinks.Add(newShortLink);
+        _applicationDbContext.SaveChanges();
+
+        return newShortLink;
+    }
 }
